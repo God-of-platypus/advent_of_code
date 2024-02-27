48dd711a3b5c1d159c1e31b3d4fa38cb9e43ac5e
stan <stanislas.martin35300@gmail.com>
2024-02-27 09:34:02 +0100

    clean: rm trash file

--- 2020/rust/day7q1/target/debug/day7.d
+++
@@ -1,1 +0,0 @@
-/home/stan/advent_of_code/2020/rust/day7/target/debug/day7: /home/stan/advent_of_code/2020/rust/day7/day7.rs
--- 2020/rust/day7q1/target/debug/deps/day7-c5d56a081e7d960a.d
+++
@@ -1,5 +0,0 @@
-/home/stan/advent_of_code/2020/rust/day7/target/debug/deps/day7-c5d56a081e7d960a: day7.rs
-
-/home/stan/advent_of_code/2020/rust/day7/target/debug/deps/day7-c5d56a081e7d960a.d: day7.rs
-
-day7.rs:
